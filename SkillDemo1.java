7a8850230b9e14326e6a09394b247240739f9f61
David Tran <tr.davidt@gmail.com>
2022-02-02 13:04:22 -0800

    Add failing test and .jar files

---
+++ SkillDemo1.java
@@ -0,0 +1,5 @@
+public class SkillDemo1 {
+  public static int add(int m, int n) {
+    return m + n;
+  }
+}
---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+  @Test
+  public void testAdd() {
+    assertEquals(1, SkillDemo1.add(1, 1));
+  }
+}
